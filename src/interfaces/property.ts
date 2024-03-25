12135326025b834f00f9f2395aebdf8659efbe30
Dominik Brych <dominik.brych@thenetw.org>
2024-03-25 12:11:29 +0100

    Typings

--- src/interfaces/property.ts
+++ src/interfaces/property.ts
@@ -1,22 +1,24 @@
-export interface IStringProperty extends Omit<ComponentFramework.PropertyTypes.StringProperty, 'type' | 'error' | 'errorMessage'> {
+type ExcludedProps = Pick<ComponentFramework.PropertyTypes.StringProperty, 'type' | 'error' | 'errorMessage' | 'formatted'>;
+
+export interface IStringProperty extends Omit<ComponentFramework.PropertyTypes.StringProperty, keyof ExcludedProps> {
     type?: string;
     error?: boolean;
     errorMessage?: string;
 }
 
-export interface IProperty extends Omit<ComponentFramework.PropertyTypes.Property, 'type' | 'error' | 'errorMessage'> {
+export interface IProperty extends Omit<ComponentFramework.PropertyTypes.Property, keyof ExcludedProps> {
     type?: string;
     error?: boolean;
     errorMessage?: string;
 }
 
-export interface ITwoOptionsProperty extends Omit<ComponentFramework.PropertyTypes.TwoOptionsProperty, 'type' | 'error' | 'errorMessage'> {
+export interface ITwoOptionsProperty extends Omit<ComponentFramework.PropertyTypes.TwoOptionsProperty, keyof ExcludedProps> {
     type?: string;
     error?: boolean;
     errorMessage?: string;
 }
 
-export interface IDecimalNumberProperty extends Omit<ComponentFramework.PropertyTypes.DecimalNumberProperty, 'type' | 'error' | 'errorMessage'> {
+export interface IDecimalNumberProperty extends Omit<ComponentFramework.PropertyTypes.DecimalNumberProperty, keyof ExcludedProps> {
     type?: string;
     error?: boolean;
     errorMessage?: string;
