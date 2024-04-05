644f89fd35267428a3ceeb38e6af9114d5b6e01a
Safija Hubljar <safija.hubljar@edu.fit.ba>
2024-04-05 13:15:45 +0200

    Decimal formatting - track outside changes

--- src/components/Decimal/Decimal.tsx
+++ src/components/Decimal/Decimal.tsx
@@ -1,65 +1,89 @@
-import { TextField  } from '@talxis/react-components/dist/components/TextField';
+import { TextField } from "@talxis/react-components/dist/components/TextField";
 import { useInputBasedComponent } from "../../hooks/useInputBasedComponent";
 import { IDecimal, IDecimalOutputs, IDecimalParameters } from "./interfaces";
-import React, {useEffect, useRef } from 'react';
-import numeral from 'numeral';
+import React, { useEffect, useRef } from "react";
+import numeral from "numeral";
 
 export const Decimal = (props: IDecimal) => {
-    const [value, setValue, onNotifyOutputChanged] = useInputBasedComponent<number|string, IDecimalParameters, IDecimalOutputs>(props);
-    const context = props.context;
-    const parameters = props.parameters;
-    const boundValue = parameters.value;
-    const ref = useRef<HTMLDivElement>(null);
-    function extractNumericPart(str:any): number|undefined {
-        const regex = /^[\d,.\s]+$/;
-        if (regex.test(str))
-        {
-        let formatedValue = numeral(str).value() || undefined;
-        return formatedValue!;
-        }else{
+  const [value, setValue, onNotifyOutputChanged] = useInputBasedComponent<
+    number | string,
+    IDecimalParameters,
+    IDecimalOutputs
+  >(props);
+  const context = props.context;
+  const parameters = props.parameters;
+  const boundValue = parameters.value;
+  const ref = useRef<HTMLDivElement>(null);
 
-            return value as number;
-        } 
-            
+  const extractNumericPart = (str: any): number | undefined => {
+    const regex = /^[\d,.\s]+$/;
+    if (regex.test(str)) {
+      let formatedValue = numeral(str).value() || undefined;
+      return formatedValue!;
+    } 
+    else {
+      return value as number;
     }
+  };
 
-    useEffect(()=>{
-        if(boundValue.raw){
-            setValue(context.formatting.formatInteger(context.formatting.formatDecimal(boundValue.raw!,boundValue.attributes?.Precision)as unknown as number)as unknown as number);
-        }
-    },[]);
+  const formatNumber = (decimalVal: number): number => {
+    if (decimalVal && !isNaN(decimalVal)) {
+      const formatedDecimalValue = context.formatting.formatDecimal(+decimalVal);
+      return context.formatting.formatInteger(formatedDecimalValue as unknown as number) as unknown as number;
+    } 
+    else {
+      return decimalVal;
+    }
+  };
 
-    return  <TextField
-    readOnly={context.mode.isControlDisabled}
-            autoFocus={parameters.AutoFocus?.raw}
-            elementRef={ref}
-            borderless={parameters.EnableBorder?.raw === false}
-            errorMessage={boundValue.error? boundValue.errorMessage : '' }
-            deleteButtonProps={parameters.EnableDeleteButton?.raw === true ? {
-                key: 'delete',
-                showOnlyOnHover: true,
-                iconProps: {
-                    iconName: 'Delete'
-                },
-                onClick: () => setValue(null)
-            } : undefined}
-            clickToCopyProps={parameters.EnableCopyButton?.raw === true ? {
-                key: 'copy',
-                iconProps: {
-                    iconName: 'Copy'
-                }
-            } : undefined}
-            value={value != null ? value as unknown as string: undefined}
-            
-            onBlur={() => {
+  useEffect(() => {
+    if (boundValue.raw != null) {
+      let numericValue = extractNumericPart(boundValue.raw);
+      if (numericValue && !isNaN(numericValue)) {
+        setValue(formatNumber(numericValue));
+      }
+    }
+  }, [boundValue.raw]);
 
-               let numericValue = extractNumericPart(value as string);
-                onNotifyOutputChanged({
-                    value: numericValue
-                });
-            }}
-            onChange={(e, value) => {
-                setValue(value ?? null);
-            }} 
-    />;
+  return (
+    <TextField
+      readOnly={context.mode.isControlDisabled}
+      autoFocus={parameters.AutoFocus?.raw}
+      elementRef={ref}
+      borderless={parameters.EnableBorder?.raw === false}
+      errorMessage={boundValue.error ? boundValue.errorMessage : ""}
+      deleteButtonProps={
+        parameters.EnableDeleteButton?.raw === true
+          ? {
+              key: "delete",
+              showOnlyOnHover: true,
+              iconProps: {
+                iconName: "Delete",
+              },
+              onClick: () => setValue(null),
+            }
+          : undefined
+      }
+      clickToCopyProps={
+        parameters.EnableCopyButton?.raw === true
+          ? {
+              key: "copy",
+              iconProps: {
+                iconName: "Copy",
+              },
+            }
+          : undefined
+      }
+      value={value != null ? (value as unknown as string) : undefined}
+      onBlur={(event) => {
+        let numericValue = extractNumericPart(event.target.value);
+        onNotifyOutputChanged({
+          value: numericValue,
+        });
+      }}
+      onChange={(e, value) => {
+        setValue(value ?? null);
+      }}
+    />
+  );
 };
--- src/sandbox/index.tsx
+++ src/sandbox/index.tsx
@@ -1,81 +1,83 @@
-import { initializeIcons, Label, PrimaryButton} from '@fluentui/react';
-import { TextField as TalxisTextField } from '@talxis/react-components/dist/components/TextField';
-import { TextField as TalxisDecimalField } from '@talxis/react-components/dist/components/TextField';
-import React, {  useState } from 'react';
-import { TextField } from '../components/TextField/TextField';
-import { Context } from './mock/Context';
-import { Decimal } from '../components/Decimal/Decimal';
+import { initializeIcons, Label, PrimaryButton } from "@fluentui/react";
+import { TextField as TalxisTextField } from "@talxis/react-components/dist/components/TextField";
+import { TextField as TalxisDecimalField } from "@talxis/react-components/dist/components/TextField";
+import React, { useState } from "react";
+import { TextField } from "../components/TextField/TextField";
+import { Context } from "./mock/Context";
+import { Decimal } from "../components/Decimal/Decimal";
 
 initializeIcons();
 
 export const Sandbox: React.FC = () => {
-    const [value, setValue] = useState<string>();
-    const [decimalValue, setDecimalValue] = useState<number>();
-    const [isMounted, setIsMounted] = useState<boolean>(true);
-    const [test, setTest] = useState('');
-    const context = new Context();
-     function formatNumber(decimalVal: number):number{
-        if(decimalVal && !isNaN(decimalVal)){
-            const formatedDecimalValue = context.formatting.formatDecimal(+decimalVal);
-            return context.formatting.formatInteger(formatedDecimalValue as unknown as number) as unknown as number;
-        }else return decimalVal;
-    }
-    return (
+  const [value, setValue] = useState<string>();
+  const [decimalValue, setDecimalValue] = useState<number>();
+  const [isMounted, setIsMounted] = useState<boolean>(true);
+  const [test, setTest] = useState("");
+  const context = new Context();
+
+  return (
+    <>
+      <Label>Outside change</Label>
+      <TalxisTextField value={value} onChange={(e, value) => setValue(value)} />
+      {isMounted && (
         <>
-            <Label>Outside change</Label>
-            <TalxisTextField value={value} onChange={(e, value) => setValue(value)} />
-            {isMounted &&
-                <>
-                    <Label>Component</Label>
-                    <TextField
-                        context={new Context()}
-                        onNotifyOutputChanged={(outputs) => {
-                            setValue(outputs.value as string);
-                        }}
-                        parameters={{
-                            IsMultiLine: {
-                                raw: true
-                            },
-                            isResizable: {
-                                raw: false
-                            },
-                            EnableCopyButton: {
-                                raw: true,
-                            },
-                            value: {
-                                raw: value ?? null,
-                            }
-                        }} />
-                </>
-            }
-            <br />
-            <PrimaryButton text='Mount/Unmount component' onClick={() => setIsMounted(!isMounted)} />
-            <br />
-            <br />
-            <PrimaryButton text='Trigger rerender' onClick={() => setTest(Math.random().toString()) }/>
-            <Label>Outside changes</Label>
-            <TalxisDecimalField onChange={(e, value) => setDecimalValue(value as unknown as number)}
-              onBlur={() => {
-                setDecimalValue(formatNumber(decimalValue!));
-             }}
-            />
-            <Label>Decimal component</Label>
-            <Decimal
-                context={context}
-                parameters={{
-                    EnableBorder:{raw:true},
-                    EnableCopyButton: { raw: false },
-                    value:{
-                        raw:decimalValue ?? null,
-                        errorMessage: "Decimal column",
-                        error: false
-                    }  
-                }}
-                
-                onNotifyOutputChanged={(outputs) => {
-                    setDecimalValue(formatNumber(outputs.value!));
-                }}
-            />
+          <Label>Component</Label>
+          <TextField
+            context={new Context()}
+            onNotifyOutputChanged={(outputs) => {
+              setValue(outputs.value as string);
+            }}
+            parameters={{
+              IsMultiLine: {
+                raw: true,
+              },
+              isResizable: {
+                raw: false,
+              },
+              EnableCopyButton: {
+                raw: true,
+              },
+              value: {
+                raw: value ?? null,
+              },
+            }}
+          />
         </>
-    )
-}
+      )}
+      <br />
+      <PrimaryButton
+        text="Mount/Unmount component"
+        onClick={() => setIsMounted(!isMounted)}
+      />
+      <br />
+      <br />
+      <PrimaryButton
+        text="Trigger rerender"
+        onClick={() => setTest(Math.random().toString())}
+      />
+      <Label>Outside changes</Label>
+      <TalxisDecimalField
+        onBlur={(event) => {
+          setDecimalValue(event.target.value as unknown as number);
+        }}
+      />
+      <Label>Decimal component</Label>
+      <Decimal
+        context={context}
+        parameters={{
+          EnableBorder: { raw: true },
+          EnableCopyButton: { raw: false },
+          value: {
+            raw: decimalValue ?? null,
+            errorMessage: "Decimal column",
+            error: false,
+          },
+        }}
+        onNotifyOutputChanged={(outputs) => {
+          debugger;
+          setDecimalValue(outputs.value);
+        }}
+      />
+    </>
+  );
+};
