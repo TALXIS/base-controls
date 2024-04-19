a7cefb3e06956c955c3eab0f0933ab4b8e9a165c
Dominik Brych <dominik.brych@thenetw.org>
2024-04-19 15:29:55 +0200

    Fixed setting of DefaultValue

--- src/components/OptionSet/OptionSet.tsx
+++ src/components/OptionSet/OptionSet.tsx
@@ -3,11 +3,12 @@ import { IOptionSet } from './interfaces';
 import { useComponent } from '../../hooks';
 import { ComboBox } from '@talxis/react-components/dist/components/ComboBox';
 import { IComboBoxOption } from '@fluentui/react';
+import React, { useEffect } from 'react';
 
 export const OptionSet = (props: IOptionSet) => {
     const parameters = props.parameters;
     const boundValue = parameters.value;
-    const defaulValue = boundValue.attributes.DefaultValue.toString();
+    const defaulValue = boundValue.attributes.DefaultValue;
     const [labels, onNotifyOutputChanged] = useComponent('OptionSet', props);
     const { Options } = parameters.value.attributes;
     const context = props.context;
@@ -17,11 +18,26 @@ export const OptionSet = (props: IOptionSet) => {
     }));
 
     const handleChange = (option?: IComboBoxOption | null): void => {
-        const selectedKey = option?.key.toString();
+        let value = undefined;
+        if(option) {
+            value = parseInt(option.key as string);
+        }
         onNotifyOutputChanged({
-            value: +selectedKey!
+            value: value
         });
     };
+
+    //set the default value on first render
+    useEffect(() => {
+        //TODO: we should make sure that we always set the DefaultValue to -1 on existing records!
+        //@ts-ignore - not part of types
+        if(defaulValue !== -1 && !boundValue.raw) {
+            onNotifyOutputChanged({
+                value: defaulValue
+            })
+        }
+    }, []);
+
 
     return <ComboBox
         borderless={parameters.EnableBorder?.raw === false}
@@ -30,8 +46,8 @@ export const OptionSet = (props: IOptionSet) => {
             autoFocus: true
         }: undefined}
         readOnly={context.mode.isControlDisabled}
-        selectedKey={boundValue.raw?.toString() ?? defaulValue}
-        useComboBoxAsMenuWidth
+        selectedKey={boundValue.raw?.toString() ?? -1}
+        dropdownWidth={context.mode.allocatedWidth || undefined}
         styles={{
             root: {
                 height: context.mode.allocatedHeight || undefined,
