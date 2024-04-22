bd974465eb89037ec0d7a164596d95e21cb0506d
Dominik Brych <dominik.brych@thenetw.org>
2024-04-22 10:49:56 +0200

    Merge branch 'master' into safija.hubljar/32498.MultiSelectOptionSet

--- src/components/MultiSelectOptionSet/MultiSelectOptionSet.tsx
+++ src/components/MultiSelectOptionSet/MultiSelectOptionSet.tsx
@@ -3,11 +3,11 @@ import { IMultiSelectOptionSet } from './interfaces';
 import { useComponent } from '../../hooks';
 import { ComboBox } from '@talxis/react-components/dist/components/ComboBox';
 import { IComboBoxOption } from '@fluentui/react';
+import React from 'react';
 
-export const MultiSelectOptionset = (props: IMultiSelectOptionSet) => {
+export const MultiSelectOptionSet = (props: IMultiSelectOptionSet) => {
     const parameters = props.parameters;
     const boundValue = parameters.value;
-    const defaultValue = boundValue.attributes.DefaultValue.toString();
     const [labels, onNotifyOutputChanged] = useComponent('MultiSelectOptionSet', props);
     const { Options } = parameters.value.attributes;
     const context = props.context;;
@@ -17,24 +17,24 @@ export const MultiSelectOptionset = (props: IMultiSelectOptionSet) => {
     }));
 
     const handleChange = (option?: IComboBoxOption | null): void => {
-        if (option) {
-            const optionKey = option.key.toString();
-            const updatedSelectedKeys = new Set(boundValue.raw || []);
-            if (option.selected) {
-                updatedSelectedKeys.add(+optionKey);
-            } else {
-                updatedSelectedKeys.delete(+optionKey);
-            }
-            const updatedSelectedKeysArray = Array.from(updatedSelectedKeys);
-            
-            onNotifyOutputChanged({
-                value: updatedSelectedKeysArray.map(key => +key)
-            });
-        } else {
+        if(!option) {
             onNotifyOutputChanged({
                 value: undefined
             });
+            return;
         }
+        const optionKey = option.key.toString();
+        const updatedSelectedKeys = new Set(boundValue.raw || []);
+        if (option.selected) {
+            updatedSelectedKeys.add(+optionKey);
+        } else {
+            updatedSelectedKeys.delete(+optionKey);
+        }
+        const updatedSelectedKeysArray = Array.from(updatedSelectedKeys);
+        
+        onNotifyOutputChanged({
+            value: updatedSelectedKeysArray.map(key => +key)
+        });
     };
 
     return <ComboBox
@@ -45,7 +45,7 @@ export const MultiSelectOptionset = (props: IMultiSelectOptionSet) => {
         autoComplete="on"
         readOnly={context.mode.isControlDisabled}
         dropdownWidth={context.mode.allocatedWidth || undefined}
-        selectedKey={boundValue.raw ? boundValue.raw.map(key => key.toString()) : defaultValue}
+        selectedKey={boundValue.raw ? boundValue.raw.map(key => key.toString()) : [-1]}
         styles={{
             root: {
                 height: context.mode.allocatedHeight || undefined,
