53849fec2da7956c51614f8a29d5218a5182354b
Dominik Brych <dominik.brych@thenetw.org>
2025-03-12 11:24:33 +0100

    Removed cell height offsset

--- src/components/GridCellRenderer/styles.ts
+++ src/components/GridCellRenderer/styles.ts
@@ -7,7 +7,7 @@ export const getGridCellLabelStyles = (columnAlignment: IColumn['alignment'], da
         root: {
             display: 'flex',
             alignItems: 'center',
-            height: rowHeight + 1,
+            height: rowHeight,
             paddingLeft: 8,
             paddingRight: 8,
             justifyContent: getJustifyContent(columnAlignment),
