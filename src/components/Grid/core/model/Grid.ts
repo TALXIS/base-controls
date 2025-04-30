b094e2bd49713f2b26958cdbe9de6f0d579620c0
Dominik Brych <dominik.brych@thenetw.org>
2025-04-30 14:45:04 +0200

    Reverted change

--- src/components/Grid/core/model/Grid.ts
+++ src/components/Grid/core/model/Grid.ts
@@ -151,14 +151,14 @@ export class Grid {
                 height: this.parameters.Height?.raw
             }
         }
-        if(this._records.length <= 15) {
+        if(this._records.length <= 50) {
             return {
                 isAutoHeightEnabled: true
             }
         }
         return {
             isAutoHeightEnabled: false,
-            height: `${this.rowHeight * 15}px`
+            height: `${this.rowHeight * 50}px`
         }
 
 
