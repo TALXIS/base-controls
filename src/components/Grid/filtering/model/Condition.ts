ff28c4b087bfe58e97a10c7046ea26845988de30
Dominik Brych <dominik.brych@thenetw.org>
2025-05-29 15:51:57 +0200

    Reverted change

--- src/components/Grid/filtering/model/Condition.ts
+++ src/components/Grid/filtering/model/Condition.ts
@@ -157,11 +157,11 @@ export class Condition extends GridDependency {
                     }
                 }
                 //@ts-ignore - going from between/not between to other operators
-                if((previousOperator === DatasetConditionOperator.Between || previousOperator === DatasetConditionOperator.NotBetween) && (conditionOperator !== DatasetConditionOperator.Between && conditionOperator !== DatasetConditionOperator.NotBetween)) {
+                if((previousOperator === Data || previousOperator === 11) && (conditionOperator !== 10 && conditionOperator !== 11)) {
                     this._conditionExpression.value = "";
                 }
                 //@ts-ignore - going from other operators to between/not between
-                if((conditionOperator === DatasetConditionOperator.Between || conditionOperator === DatasetConditionOperator.NotBetween) && (previousOperator !== DatasetConditionOperator.Between && previousOperator !== DatasetConditionOperator.NotBetween)) {
+                if((conditionOperator === 10 || conditionOperator === 11) && (previousOperator !== 10 && previousOperator !== 11)) {
                     this._conditionExpression.value = []
                 }
             },
