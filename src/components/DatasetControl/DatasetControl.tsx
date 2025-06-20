05b3a688617d7cd2e570ad60d221739abce63f47
Dominik Brych <dominik.brych@thenetw.org>
2025-06-20 14:30:47 +0200

    Removed unused import

--- src/components/DatasetControl/DatasetControl.tsx
+++ src/components/DatasetControl/DatasetControl.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo, useRef } from "react";
 import { useControl } from "../../hooks";
 import { ThemeProvider } from "@fluentui/react";
 import { datasetControlTranslations } from "./translations";
