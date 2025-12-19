d47ee3ba42dbbfafa6399a265f8566f9312a9dad
Dominik Brych <dominik.brych@thenetw.org>
2025-12-19 09:52:22 +0100

    Bump @talxis/client-libraries to version 1.2512.3 and update onInitialize method signature in DatasetAdapter and VirtualDatasetAdapter

--- src/utils/dataset/adapters/DatasetAdapter.ts
+++ src/utils/dataset/adapters/DatasetAdapter.ts
@@ -35,7 +35,7 @@ interface IInputs {
 }
 
 interface IDatasetAdapterOptions {
-    onInitialize?: () => Promise<void>;
+    onInitialize?: (dataset: IDataset) => Promise<void>;
 }
 
 
@@ -76,7 +76,7 @@ export class DatasetAdapter {
             await defaultAction(parameters);
             //sets columns after preload
             this._dataset.setColumns(this._getColumns());
-            await this._options?.onInitialize?.();
+            await this._options?.onInitialize?.(this.getDataset());
         });
         if (this._getHeight() === '100%') {
             this._container.classList.add(this._getFullHeightStyles())
