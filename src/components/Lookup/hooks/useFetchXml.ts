b72c4590aa422759a3ffc1c0285d250e9e1b35c9
Dominik Brych <dominik.brych@thenetw.org>
2026-01-12 16:19:21 +0100

    fix: Refactor applyLookupQuery to handle placeholder conditions and clean up XML structure

--- src/components/Lookup/hooks/useFetchXml.ts
+++ src/components/Lookup/hooks/useFetchXml.ts
@@ -3,14 +3,14 @@ import { IEntity } from "../interfaces";
 import { FetchXmlBuilder } from "@talxis/client-libraries";
 
 export const useFetchXml = (context: ComponentFramework.Context<any>): [
-    (viewId: string) => Promise<{fetchxml: string, layoutjson: string}>,
+    (viewId: string) => Promise<{ fetchxml: string, layoutjson: string }>,
     (entity: IEntity, fetchXml: string, query: string) => Promise<string>
 ] => {
     const cachedFetchXml = useRef<{
         [viewId: string]: Promise<any>
     }>({});
 
-    const get = async (viewId: string): Promise<{fetchxml: string, layoutjson: string}> => {
+    const get = async (viewId: string): Promise<{ fetchxml: string, layoutjson: string }> => {
         if (!cachedFetchXml.current[viewId]) {
             cachedFetchXml.current[viewId] = context.webAPI.retrieveRecord('savedquery', viewId, '?$select=fetchxml,layoutjson');
         }
@@ -18,15 +18,38 @@ export const useFetchXml = (context: ComponentFramework.Context<any>): [
         return cachedFetchXml.current[viewId];
     }
     const applyLookupQuery = async (entity: IEntity, fetchXml: string, query: string): Promise<string> => {
-        if (!query) {
-            return fetchXml
+        const parser = new DOMParser();
+        const parsedFetchXml = parser.parseFromString(fetchXml, "application/xml");
+        const conditions = parsedFetchXml.getElementsByTagName("condition");
+        const placeholderConditions: Element[] = [];
+        for (const cond of conditions) {
+            if (cond.getAttribute('value') === "{0}") {
+                cond.setAttribute('value', query);
+                placeholderConditions.push(cond);
+            }
         }
-        const metadata = await entity.metadata;
-        const xmlObject = FetchXmlBuilder.fetch.fromXml(fetchXml);
-        xmlObject.entity.addFilter(new FetchXmlBuilder.filter(FetchXmlBuilder.FilterType.Or, [
-            new FetchXmlBuilder.condition(metadata.PrimaryNameAttribute, FetchXmlBuilder.Operator.Like, [new FetchXmlBuilder.value(`%${query}%`)])
-        ]))
-        return xmlObject.toXml();
+        if (!query) {
+            for (const cond of placeholderConditions) {
+                const parentFilter = cond.parentElement;
+                parentFilter?.removeChild(cond);
+                if(parentFilter?.children.length === 0) {
+                    const grandParent = parentFilter.parentElement;
+                    grandParent?.removeChild(parentFilter);
+                }
+            }
+            return new XMLSerializer().serializeToString(parsedFetchXml);
+        }
+        if (placeholderConditions.length > 0) {
+            return new XMLSerializer().serializeToString(parsedFetchXml);
+        }
+        else {
+            const metadata = await entity.metadata;
+            const xmlObject = FetchXmlBuilder.fetch.fromXml(fetchXml);
+            xmlObject.entity.addFilter(new FetchXmlBuilder.filter(FetchXmlBuilder.FilterType.Or, [
+                new FetchXmlBuilder.condition(metadata.PrimaryNameAttribute, FetchXmlBuilder.Operator.Like, [new FetchXmlBuilder.value(`%${query}%`)])
+            ]))
+            return xmlObject.toXml();
+        }
     }
     return [get, applyLookupQuery]
 }
