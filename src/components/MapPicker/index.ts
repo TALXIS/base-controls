f4152e3a6e12044450f418300319e21e070ab322
Matej Samler <matej.samler@thenetw.org>
2026-02-20 19:38:34 +0100

    Add MapPicker component with Google Maps integration
    - Introduced MapPicker component.
    - Added GoogleMapsProvider for rendering Google Maps.

---
+++ src/components/MapPicker/index.ts
@@ -0,0 +1,3 @@
+export * from './interfaces';
+export * from './components/MapPicker';
+export * from './providers';
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export * from './Form';
 export * from './Grid';
 export * from './GridCellRenderer';
 export * from './Lookup';
+export * from './MapPicker';
 export * from './MultiSelectOptionSet';
 export * from './NestedControlRenderer';
 export * from './Notifications';
