32c509d3c0b7a5743b53b62382b18d0bc3cd827e
Matej Samler <matej.samler@thenetw.org>
2026-02-24 16:42:19 +0100

    Renamed MapPicker to Map

--- src/components/Map/providers/GoogleMaps/GoogleMapsProvider.tsx
+++ src/components/Map/providers/GoogleMaps/GoogleMapsProvider.tsx
@@ -8,6 +8,8 @@ export interface IGoogleMapsConfig {
 }
 
 const GoogleMapsMap = (props: IMapProviderProps & { apiKey: string }) => {
+
+    // This sometimes doesn't work and defaults to (0, 0)
     const center = useMemo(() => {
         if (props.locations.length === 0) return { lat: 0, lng: 0 };
         const lat = props.locations.reduce((s, l) => s + l.latitude, 0) / props.locations.length;
@@ -21,7 +23,7 @@ const GoogleMapsMap = (props: IMapProviderProps & { apiKey: string }) => {
                 <Map defaultCenter={center} defaultZoom={13} disableDefaultUI style={{ width: '100%', height: '400px' }}>
                     {props.locations.map((location) => (
                         // To use AdvancedMarker, we need mapid
-                        <Marker key={location.id} position={{ lat: location.latitude, lng: location.longitude }} />
+                        <Marker key={location.id} position={{ lat: location.latitude, lng: location.longitude }}  />
                     ))}
                 </Map>
             </div>
