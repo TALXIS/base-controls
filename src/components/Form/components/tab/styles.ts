2adefd768d6961f1cd0e8d51c698a9ac14fc5d72
Dominik Brych <dominik.brych@thenetw.org>
2026-07-21 16:26:50 +0200

    fix: Update tab styles to set height to '0' instead of minHeight for better layout control

--- src/components/Form/components/tab/styles.ts
+++ src/components/Form/components/tab/styles.ts
@@ -5,7 +5,7 @@ export const getTabStyles = () => {
         tab: {
             gap: 12,
             flexGrow: 1,
-            minHeight: '0',
+            height: '0',
             overflow: 'auto'
         }
     });
