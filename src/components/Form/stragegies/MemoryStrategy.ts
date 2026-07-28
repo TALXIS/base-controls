9a60021135559da26a583fedc9b4cacb2ce1dbcf
Dominik Brych <dominik.brych@thenetw.org>
2026-07-28 14:27:37 +0200

    feat: Update onSave method to accept IOnSaveParams for improved data handling

--- src/components/Form/stragegies/MemoryStrategy.ts
+++ src/components/Form/stragegies/MemoryStrategy.ts
@@ -1,5 +1,5 @@
 import { IColumn, IRecordSaveOperationResult } from "@talxis/client-libraries";
-import { IFormStrategy, IOnLoadResult } from "./interfaces";
+import { IFormStrategy, IOnLoadResult, IOnSaveParams } from "./interfaces";
 
 export interface IMemoryStrategyParams {
     columns: IColumn[];
@@ -39,7 +39,9 @@ export class MemoryStrategy implements IFormStrategy {
      * Because `this._data` is the same reference passed via the constructor,
      * the caller's original object is also updated.
      */
-    public async onSave(data: {[key: string]: any}): Promise<IRecordSaveOperationResult> {
+    public async onSave(params: IOnSaveParams): Promise<IRecordSaveOperationResult> {
+        const { data } = params;
+
         Object.assign(this._data, data);
         return {
             success: true,
