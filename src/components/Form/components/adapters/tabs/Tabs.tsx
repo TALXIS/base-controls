ee89b1f7365a8374582105a9704fd220196062da
Dominik Brych <dominik.brych@thenetw.org>
2026-07-31 14:16:59 +0200

    refactor: Remove unused pcfContext and debugging log from Tabs component

--- src/components/Form/components/adapters/tabs/Tabs.tsx
+++ src/components/Form/components/adapters/tabs/Tabs.tsx
@@ -1,10 +1,7 @@
-import { usePcfContext } from "@utils";
 import { FormUi, IFormTabsProps, TabLikeChild } from "@components/Form/components/ui";
 
 export type { IFormTabsProps, TabLikeChild } from "@components/Form/components/ui";
 
 export const Tabs = (props: IFormTabsProps) => {
-    const pcfContext = usePcfContext();
-    console.log("PCF Context in Tabs:", pcfContext); // Debugging line to check the context value
     return <FormUi.Tabs {...props} />;
 };
