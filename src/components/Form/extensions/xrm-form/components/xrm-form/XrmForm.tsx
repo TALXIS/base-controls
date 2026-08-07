8efab04205d10c1a57b114b31a5cb889aad9bf95
Dominik Brych <dominik.brych@thenetw.org>
2026-08-07 11:11:33 +0200

    fix: clean up xrm form and restore tabs ui exports

--- src/components/Form/extensions/xrm-form/components/xrm-form/XrmForm.tsx
+++ src/components/Form/extensions/xrm-form/components/xrm-form/XrmForm.tsx
@@ -14,8 +14,6 @@ import { IFormApi } from '@components/Form/interfaces';
 import { IFormApiInternal } from '@components/Form/internal/FormApi';
 import { XrmFormComponentsContext } from './context';
 import { XrmFormComponents } from './components';
-import { TabComponents } from '@components/Form/components/ui';
-import { Pivot } from '@components/Form/components/ui/tabs/components/pivot/Pivot';
 
 
 export const XrmForm = (props: IXrmFormProps) => {
@@ -74,11 +72,3 @@ const XrmFormInternal = ({
         </XrmFormContext.Provider>
     </FormXmlContext.Provider>
 }
-
-<XrmForm components={{
-    tabs: {
-        onRenderTabs: (props) => <Pivot {...props} components={{
-            
-        }} />
-    }
-}} />
